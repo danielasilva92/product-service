cfc65439f0917438e95fa659bb0ac38cf623cff3
Daniela Silva <daniela92_@hotmail.de>
2026-06-06 10:03:31 +0200

    made it AWS ready

--- src/main/java/se/jensen/daniela/fakestoreapi/config/SecurityConfig.java
+++ src/main/java/se/jensen/daniela/fakestoreapi/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package se.jensen.daniela.fakestoreapi.config;
 
 
-import org.springframework.boot.servlet.filter.OrderedHiddenHttpMethodFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -28,7 +27,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, OrderedHiddenHttpMethodFilter hiddenHttpMethodFilter) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
@@ -38,7 +37,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/products").permitAll()
                         .requestMatchers(HttpMethod.GET, "/products/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/health").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/products/fetch").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/products/fetch").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter,
